2ddcf0c3a59881f8dea591c0dc65f3c57dec0e2a
Erin Xue <erin.jing.xue@gmail.com>
2020-01-03 15:58:40 -0800

    initiate react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>initiate gnews reader... (under construction)</p>
+    </div>
+  );
+}
+
+export default App;
